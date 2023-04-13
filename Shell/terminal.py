4bb24ad1e20f4ecc8cf7eb7ec9e62395a6b69838
1mag1n33 <kopeccarson@gmail.com>
2023-04-13 14:26:01 -0400

    c

--- Shell/terminal.py
+++ Shell/terminal.py
@@ -1,6 +1,6 @@
-import cmd
 import os
-from importlib import import_module
+import cmd
+import importlib
 
 class Terminal(cmd.Cmd):
     intro = "Welcome to the 1mag1n33 Terminal. Type help or ? to list commands.\n"
@@ -8,17 +8,6 @@ class Terminal(cmd.Cmd):
 
     def __init__(self):
         super().__init__()
-        self.load_commands()
-
-    def load_commands(self):
-        command_dir = os.path.join(os.path.dirname(__file__), 'commands')
-        for filename in os.listdir(command_dir):
-            if filename.endswith('.py') and filename != '__init__.py':
-                module_name = filename[:-3]
-                module = import_module(f'shell.commands.{module_name}')
-                command_name = f'do_{module_name}'
-                command_func = getattr(module, command_name)
-                setattr(Terminal, command_name, command_func)
 
     def do_help(self, args):
         """List available commands with "help" or detailed help with "help cmd"."""
@@ -32,3 +21,13 @@ class Terminal(cmd.Cmd):
         else:
             commands = [cmd[3:] for cmd in dir(self) if cmd.startswith('do_')]
             print('\n'.join(commands))
+
+    def load_commands(self):
+        commands_dir = os.path.join(os.getcwd(), 'shell\commands')
+        for filename in os.listdir(commands_dir):
+            if filename.endswith('.py'):
+                module_name = filename[:-3]
+                module = importlib.import_module(f'shell.commands.{module_name}')
+                command_name = module_name.replace('_', '-')
+                command_func = getattr(module, f'do_{module_name}')
+                setattr(Terminal, f'do_{command_name}', command_func)
--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from shell.terminal import Terminal
 
 if __name__ == '__main__':
     term = Terminal()
+    term.load_commands()
     term.cmdloop()
