0e0eaf19de6a6493b699308d3501f3de9b890dc2
1mag1n33 <kopeccarson@gmail.com>
2023-04-18 23:40:00 -0400

    wda

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from src.other.terminal import Terminal
 
+def exit():
+    print('Exiting the terminal...')
+    raise SystemExit
+
 if __name__ == '__main__':
     term = Terminal()
-    term.load_commands()
     while True:
         try:
             term.cmdloop()
             break
         except KeyboardInterrupt:
             print("\nKeyboardInterrupt")
-            term.do_exit()
+            exit()
--- src/other/terminal.py
+++ src/other/terminal.py
@@ -13,34 +13,13 @@ class Terminal(cmd.Cmd):
         # Dictionary to hold the commands organized by folder
         self.commands_by_folder = {}
 
+        # Load the commands
+        self.load_commands()
+
     def load_commands(self):
-        package = 'src.commands'
-        for _, module_name, ispkg in pkgutil.iter_modules([package.replace('.', '/')]):
-            if ispkg:
-                # If the module is a package, recursively load commands from it
-                subpackage = f"{package}.{module_name}"
-                for root, dirs, files in os.walk(subpackage.replace('.', '/')):
-                    for file in files:
-                        if file.endswith('.py'):
-                            # Import the module
-                            module_path = os.path.join(root, file)[:-3].replace('/', '.')
-                            module = importlib.import_module(module_path)
-                            # Get the function name from the module name
-                            function_name = f'do_{module_path.split(".")[-1]}'
-                            # Check if the function exists in the module
-                            if hasattr(module, function_name):
-                                # If it exists, get the function and add it as a command
-                                command_func = getattr(module, function_name)
-                                setattr(Terminal, command_func.__name__, command_func)
-                                # Add the command to the dictionary of commands by folder
-                                folder_name = os.path.relpath(root, subpackage.replace('.', '/'))
-                                self.commands_by_folder.setdefault(folder_name, [])
-                                self.commands_by_folder[folder_name].append(command_func.__name__)
-                                
-                                help_func = getattr(module, f'help_{module_path}', None)
-                                if help_func:
-                                    setattr(self.__class__, f'help_{module_path}', help_func)
-            else:
+        package = 'commands'
+        for _, module_name, ispkg in pkgutil.iter_modules([package]):
+            if not ispkg:
                 # If the module is not a package, import it directly
                 # Import the module
                 module = importlib.import_module(f'{package}.{module_name}')
@@ -52,13 +31,15 @@ class Terminal(cmd.Cmd):
                     command_func = getattr(module, function_name)
                     setattr(Terminal, command_func.__name__, command_func)
                     # Add the command to the dictionary of commands by folder
-                    self.commands_by_folder.setdefault('', [])
-                    self.commands_by_folder[''].append(command_func.__name__)
+                    folder_name = os.path.relpath(os.path.dirname(module.__file__), package)
+                    self.commands_by_folder.setdefault(folder_name, [])
+                    self.commands_by_folder[folder_name].append(command_func.__name__)
                     
+                    # Add the help command for the command
                     help_func = getattr(module, f'help_{module_name}', None)
                     if help_func:
-                        setattr(self.__class__, f'help_{module_name}', help_func)
-                    
+                        setattr(self.__class__, f'help_{command_func.__name__}', help_func)
+
     # Help command
     def do_help(self, args=None):
         """
@@ -78,15 +59,7 @@ class Terminal(cmd.Cmd):
             # Display list of available commands organized by folder
             print("Available commands:")
             for folder, commands in self.commands_by_folder.items():
-                print(f"{folder or 'root'}:")
+                if folder:
+                    print(f"\n{folder}:")
                 for command in commands:
-                    print(f"\t- {command}")
-                    
-    
-    # Exit command
-    def do_exit(self, args=None):
-        """
-        Exits the terminal
-        """
-        print('Exiting the terminal...')
-        raise SystemExit
+                    print(f"\t{command}")
