dcf0e56daec653b9c8b660e8a215c8ee596f4932
1mag1n33 <kopeccarson@gmail.com>
2023-05-02 17:17:21 -0400

    website

--- src/other/commands/util/website.py
+++ src/other/commands/util/website.py
@@ -1,19 +1,31 @@
+import argparse
+import os
+import shutil
+import zipfile
+import requests
+from tqdm import tqdm
+
 def do_website(self, args):
-    import argparse
-    import shutil
-    import os
-    import datetime
-    
     parser = argparse.ArgumentParser(prog='website')
     subparsers = parser.add_subparsers(dest='command')
-    
     subparsers.add_parser('flask')
-    
     parsed_args = parser.parse_args(args.split())
-    
-    
+
     if parsed_args.command == 'flask':
-        src_dir = os.path.abspath('src/other/support_files/website/flask')
-        timestamp = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-        dest_dir = os.path.join(os.getcwd(), f'flask_{timestamp}')
-        shutil.copytree(src_dir, dest_dir)
+        url = 'https://terminaldevlopment.000webhostapp.com/src/flask.zip'
+        filename = url.split('/')[-1]
+        response = requests.get(url, stream=True)
+        total_size = int(response.headers.get('content-length', 0))
+        block_size = 1024 # 1 Kibibyte
+        progress_bar = tqdm(total=total_size, unit="iB", unit_scale=True)
+
+        with open(filename, 'wb') as f:
+            for data in response.iter_content(block_size):
+                progress_bar.update(len(data))
+                f.write(data)
+        progress_bar.close()
+
+        with zipfile.ZipFile(filename, 'r') as zip_ref:
+            zip_ref.extractall(os.getcwd())
+
+        os.remove(filename)
