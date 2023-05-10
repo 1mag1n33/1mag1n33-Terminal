07e30a7ddfbb37d1fbd131958cafbfa2af0a2ba2
1mag1n33 <kopeccarson@gmail.com>
2023-05-09 20:14:01 -0400

    soon adding history

--- src/cli/util/command_shortner.py
+++ src/cli/util/command_shortner.py
@@ -1,15 +1,23 @@
+import argparse
 import os
 
-# get the path of the file to be checked
-file_path = os.getcwd()
+# Create the argument parser
+parser = argparse.ArgumentParser(description='Process a file based on its extension')
+parser.add_argument('filename', type=str, help='The name of the file to process')
 
-# get the extension of the file
-file_ext = os.path.splitext(file_path)[1]
-class command_short():
-    # check if the file has .py extension
-    if file_ext == '.py':
-        # execute the command here
-        print("Running the Python file")
-    else:
-        # do something else
-        print("File extension is not .py")
+# Parse the arguments
+args = parser.parse_args()
+
+# Get the extension of the file
+file_ext = os.path.splitext(args.filename)[1]
+
+# Check the extension and execute the corresponding command
+if file_ext == '.py':
+    # Execute the command to run a Python script
+    print(f"Running the Python script:")
+elif file_ext == '.txt':
+    # Execute the command to display the contents of a text file
+    print(f"Displaying the contents of the text file: {args.filename}")
+else:
+    # File extension not recognized
+    print(f"Error: file extension not recognized for file {args.filename}")
